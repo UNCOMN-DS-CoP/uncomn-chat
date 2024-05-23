c327f8ae9c10e4c6fa8828a80b33e34344afba59
edabbs <edabbs@uncomn.com>
2024-05-23 13:41:20 -0500

    updated title and icons

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
       },
       injectRegister: 'auto',
       manifest: {
-        name: 'Bedrock Claude Chat',
-        short_name: 'Bedrock Chat',
+        name: 'Uncomn Chat JARVIS',
+        short_name: 'JARVIS',
         description: 'AWS-native chatbot using Bedrock + Claude',
         start_url: '/index.html',
         display: 'standalone',
