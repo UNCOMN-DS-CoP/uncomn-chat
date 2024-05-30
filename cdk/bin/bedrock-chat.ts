556b4efcd09283928cb7bdac85da2af534c22a2d
serappold <rappold.scott@gmail.com>
2024-05-29 22:19:36 -0500

    updated bedrock-chat.ts back to east-1 as the WAF for CloudFront can only be created in that region

--- cdk/bin/bedrock-chat.ts
+++ cdk/bin/bedrock-chat.ts
@@ -39,7 +39,7 @@ const ENABLE_MISTRAL: boolean = app.node.tryGetContext("enableMistral");
 const waf = new FrontendWafStack(app, `FrontendWafStack`, {
   env: {
     // account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: "us-west-2",
+    region: "us-east-1",
   },
   allowedIpV4AddressRanges: ALLOWED_IP_V4_ADDRESS_RANGES,
   allowedIpV6AddressRanges: ALLOWED_IP_V6_ADDRESS_RANGES,
